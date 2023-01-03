db07abb6865bee2f57a7498aed3d10551b4f5795
Tim Kelley <timkelley683@gmail.com>
2023-01-03 06:57:00 -0800

    config file with env vars

---
+++ config.js
@@ -0,0 +1,33 @@
+"use strict";
+
+require("dotenv").config();
+require("colors");
+
+const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
+
+const PORT = +process.env.PORT || 3001;
+
+// Use dev datatbase, testing db, or via env var, production db
+function getDatabaseUri() {
+    return (process.env.NODE_ENV === "test")
+    ? "juxtapose_test"
+    : process.env.DATABASE_URL || "juxtapose";
+}
+
+// Speed up bcrypt during tests
+
+const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
+
+console.log("Jobly Config:".green);
+console.log("SECRET KEY:".yellow, SECRET_KEY);
+console.log("PORT:".yellow, PORT.toString());
+console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
+console.log("Database:".yellow, getDatabaseUri());
+console.log("----------");
+
+module.exports = {
+    SECRET_KEY,
+    PORT,
+    BCRYPT_WORK_FACTOR,
+    getDatabaseUri,
+};
