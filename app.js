66f12d41d8bddfaef26a6b27383270c4fd001d51
Tim Kelley <timkelley683@gmail.com>
2023-01-03 06:56:30 -0800

    commented out auth for now

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 
 const { NotFoundError } = require("./expressError");
 
-const { authenticateJWT } = require("./middleware/auth");
+// const { authenticateJWT } = require("./middleware/auth");
 
 
 
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
-app.use(authenticateJWT);
+// app.use(authenticateJWT);
 
 
 
